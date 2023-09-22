a418c8f54cc49c23db6aa06af902006e077e3a8e
Edvard Ahlsén <edvard.ahlsen@gmail.com>
2023-09-22 18:17:38 +0200

    tests and implementation for solving the challenge

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,62 +1,113 @@
-import { readInput, parseInput } from '../src/main.js';
+import {
+  InputData,
+  readInput,
+  parseInput,
+  BingoBoard,
+  BingoGameManager,
+} from '../src/main.js';
 
-describe('test', () => {
+describe('squid game tests', () => {
   let testInput;
 
   beforeAll(() => {
     testInput = readInput('test.in');
   });
 
-  it('has access to the correct test input', () => {
-    expect(typeof testInput).toBe('string');
+  describe('input and parsing', () => {
+    it('has access to the correct test input', () => {
+      expect(typeof testInput).toBe('string');
+    });
+
+    it('parses the input correctly', () => {
+      const inputData = parseInput(testInput);
+      expect(inputData.drawnNumbers.length).toBeGreaterThan(0);
+      expect(inputData.boards.length).toBeGreaterThan(0);
+      expect(inputData.boards[0].length).toEqual(5);
+      expect(inputData.boards[0][0].length).toEqual(5);
+    });
+
+    it('should parse the first board correctly into its data structure', () => {
+      const inputData = parseInput(testInput);
+      const firstBoard = new BingoBoard(inputData.boards[0]);
+      const gameManager = new BingoGameManager(inputData.boards);
+      expect(Object.values(firstBoard.numbers).length).toEqual(25);
+      expect(Object.values(firstBoard.lines).length).toEqual(10);
+      expect(Object.values(gameManager.boards).length).toBeGreaterThan(2);
+    });
   });
 
-  it('parses the input correctly', () => {
-    const inputData = parseInput(testInput);
-    expect(inputData.drawnNumbers.length).toBeGreaterThan(0);
-    expect(inputData.boards.length).toBeGreaterThan(0);
-    expect(inputData.boards[0].length).toEqual(5);
-    expect(inputData.boards[0][0].length).toEqual(5);
+  describe('game functionality', () => {
+    let inputData: InputData;
+    let gameManager: BingoGameManager;
+    beforeAll(() => {
+      inputData = parseInput(testInput);
+    });
+    beforeEach(() => {
+      gameManager = new BingoGameManager(inputData.boards);
+    });
+    it('should be able to check and mark a number in the map of numbers', () => {
+      expect(gameManager.isNumberMarked(1)).toBeFalsy();
+      gameManager.markNumber(1);
+      expect(gameManager.isNumberMarked(1)).toBeTruthy();
+    });
+
+    it('should be able to return lines that contain a certain number from a board', () => {
+      const firstBoard = gameManager.boards[0];
+      expect(firstBoard.getLinesContainingNumber(0).length).toEqual(2);
+      expect(firstBoard.getLinesContainingNumber(30).length).toEqual(0);
+    });
+
+    it('should be able to tell whether a board is in a winning state', () => {
+      // First line 22 13 17 11 0
+      // First column 22 8 21 6 1
+      const firstBoard = gameManager.boards[0];
+      expect(gameManager.isBoardWinning(firstBoard)).toBeFalsy();
+      gameManager.markNumber(22);
+      gameManager.markNumber(13);
+      gameManager.markNumber(17);
+      gameManager.markNumber(11);
+      gameManager.markNumber(11);
+      expect(gameManager.isBoardWinning(firstBoard)).toBeFalsy();
+      gameManager.markNumber(0);
+      expect(gameManager.isBoardWinning(firstBoard)).toBeTruthy();
+    });
+
+    it('should be able give the correct last winning board', () => {
+      const squareWithNumber3 = gameManager.getLastWinner(
+        inputData.drawnNumbers,
+      ).board.numbers[3]; // Should be second board: top left square
+      expect(squareWithNumber3.col).toBe(0);
+      expect(squareWithNumber3.row).toBe(0);
+    });
+
+    it('should be able to calculate the sum of the unmarked numbers correctly', () => {
+      const { board, lastMarkedNumber } = gameManager.getLastWinner(
+        inputData.drawnNumbers,
+      );
+      expect(gameManager.getUnmarkedSum(board)).toEqual(148);
+      expect(lastMarkedNumber).toEqual(13);
+    });
+
+    it('should be able to calculate the score of a winning board, given the last number', () => {
+      const { board, lastMarkedNumber } = gameManager.getLastWinner(
+        inputData.drawnNumbers,
+      );
+      expect(lastMarkedNumber).toEqual(13);
+      expect(gameManager.scoreBoard(board, lastMarkedNumber)).toEqual(1924);
+    });
+  });
+
+  describe('the real deal', () => {
+    it('gives me the answer', () => {
+      const realInput = readInput('real.in');
+      const { drawnNumbers, boards } = parseInput(realInput);
+      const gameManager = new BingoGameManager(boards);
+      const { board, lastMarkedNumber } =
+        gameManager.getLastWinner(drawnNumbers);
+      const answer = gameManager.scoreBoard(board, lastMarkedNumber);
+
+      console.log(`Answer: ${answer}`);
+      expect(answer).toBeTruthy();
+    });
   });
 });
-
-// describe('greeter function', () => {
-//   const name = 'John';
-//   let hello: string;
-
-//   let timeoutSpy: jest.SpyInstance;
-
-//   // Act before assertions
-//   beforeAll(async () => {
-//     // Read more about fake timers
-//     // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-//     // Jest 27 now uses "modern" implementation of fake timers
-//     // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
-//     // https://github.com/facebook/jest/pull/5171
-//     jest.useFakeTimers();
-//     timeoutSpy = jest.spyOn(global, 'setTimeout');
-
-//     const p: Promise<string> = greeter(name);
-//     jest.runOnlyPendingTimers();
-//     hello = await p;
-//   });
-
-//   // Teardown (cleanup) after assertions
-//   afterAll(() => {
-//     timeoutSpy.mockRestore();
-//   });
-
-//   // Assert if setTimeout was called properly
-//   it('delays the greeting by 2 seconds', () => {
-//     expect(setTimeout).toHaveBeenCalledTimes(1);
-//     expect(setTimeout).toHaveBeenLastCalledWith(
-//       expect.any(Function),
-//       Delays.Long,
-//     );
-//   });
-
-//   // Assert greeter result
-//   it('greets a user with `Hello, {name}` message', () => {
-//     expect(hello).toBe(`Hello, ${name}`);
-//   });
-// });
--- src/main.ts
+++ src/main.ts
@@ -28,16 +28,100 @@ export function parseInput(inputString: string): InputData {
   return { drawnNumbers, boards };
 }
 
-type InputData = {
+export type InputData = {
   drawnNumbers: number[];
   boards: number[][][];
 };
 
-// type Board = {
-//   squares: Square[];
-// };
+class BingoSquare {
+  constructor(
+    public row: number,
+    public col: number,
+    public number: number,
+  ) {}
+}
+export class BingoBoard {
+  numbers: { [key: number]: BingoSquare } = {};
+  lines: BingoSquare[][] = [];
 
-// type Square = {
-//   row: number;
-//   col: number;
-// };
+  constructor(rawBoard: number[][]) {
+    rawBoard.forEach((row, rowIndex) =>
+      row.forEach(
+        (n, colIndex) =>
+          (this.numbers[n] = new BingoSquare(rowIndex, colIndex, n)),
+      ),
+    );
+
+    const squares = Object.values(this.numbers);
+    for (let i = 0; i < 5; i++) {
+      this.lines.push(squares.filter((square) => square.col == i));
+      this.lines.push(squares.filter((square) => square.row == i));
+    }
+  }
+
+  getLinesContainingNumber(x: number): BingoSquare[][] {
+    const squareWithNumber = this.numbers[x];
+    if (!squareWithNumber) {
+      return [];
+    } else {
+      return this.lines.filter((line) => line.includes(squareWithNumber));
+    }
+  }
+}
+
+export class BingoGameManager {
+  markedNumbers: number[] = [];
+  boards: BingoBoard[] = [];
+
+  constructor(boardsData: number[][][]) {
+    boardsData.forEach((boardData) =>
+      this.boards.push(new BingoBoard(boardData)),
+    );
+  }
+
+  markNumber(x: number): void {
+    this.markedNumbers.push(x);
+  }
+
+  isNumberMarked(x: number): boolean {
+    return this.markedNumbers.includes(x);
+  }
+
+  isLineWinning(line: BingoSquare[]): boolean {
+    return line.every((square) => this.isNumberMarked(square.number));
+  }
+  isBoardWinning(board: BingoBoard): boolean {
+    return board.lines.some((line) => this.isLineWinning(line));
+  }
+
+  getLastWinner(drawnNumbers: number[]): {
+    board: BingoBoard;
+    lastMarkedNumber: number;
+  } {
+    drawnNumbers.forEach((number) => {
+      this.markNumber(number);
+    });
+
+    while (this.markedNumbers.length > 0) {
+      const lastMarkedNumber = this.markedNumbers.pop();
+      for (const board of this.boards) {
+        if (!this.isBoardWinning(board)) {
+          this.markedNumbers.push(lastMarkedNumber);
+          return { board, lastMarkedNumber };
+        }
+      }
+    }
+    return null;
+  }
+
+  getUnmarkedSum(board: BingoBoard): number {
+    return Object.values(board.numbers)
+      .filter((square) => !this.isNumberMarked(square.number))
+      .reduce((result, current) => result + current.number, 0);
+  }
+
+  scoreBoard(board: BingoBoard, lastMarkedNumber: number): number {
+    const unmarkedSum = this.getUnmarkedSum(board);
+    return unmarkedSum * lastMarkedNumber;
+  }
+}
